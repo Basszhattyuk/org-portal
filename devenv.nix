03d5e71a9a27cdeff62ebfb85a152d43df84a6fc
leswell <simofilevente+git@gmail.com>
2025-12-09 02:02:26 +0200

    chore(local-dev): devenv run script added

--- devenv.nix
+++ devenv.nix
@@ -2,5 +2,15 @@
 {
   packages = [
     pkgs.zola
+    pkgs.unixtools.ifconfig
   ];
+
+  scripts = {
+    run.exec = 
+      let
+        # zola-port = "1353";
+        zola-port = "1111";
+      in
+      ''zola serve -p ${zola-port} --interface $(ip -4 addr show wlo1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')'';
+  };
 }
